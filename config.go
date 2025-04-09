eeeb5b77f23acf7f335a80b59daa144552f2398c
SeriousPassenger <161115168+SeriousPassenger@users.noreply.github.com>
2025-04-10 00:17:00 +0300

    initial configuration setup

---
+++ config.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type RpcConfig struct {
+	Url     string `toml:"url"`     // URL for the RPC server
+	Delay   int    `toml:"delay"`   // Delay between requests in milliseconds
+	Timeout int    `toml:"timeout"` // Timeout for requests in seconds
+	Retry   int    `toml:"retry"`   // Number of retries for failed request
+}
+
+const (
+	SampleConfigPath = "config.toml.sample"
+
+	// Default values for the RPC configuration
+	DefaultRpcUrl     = "http://localhost:8080" // Default URL for the RPC server
+	DefaultRpcDelay   = 1000                    // Default delay between requests in milliseconds
+	DefaultRpcTimeout = 5                       // Default timeout for requests in seconds
+	DefaultRpcRetry   = 3                       // Default number of retries for failed requests
+)
+
+type Config struct {
+	Rpc RpcConfig `toml:"rpc"` // RPC configuration
+}
+
+func createSampleConfig() error {
+	sampleConfig := new(Config)
+
+	sampleConfig.Rpc.Url = DefaultRpcUrl
+	sampleConfig.Rpc.Delay = DefaultRpcDelay
+	sampleConfig.Rpc.Timeout = DefaultRpcTimeout
+	sampleConfig.Rpc.Retry = DefaultRpcRetry
+
+	// Marshal the sample configuration to TOML format
+	configData, err := toml.Marshal(sampleConfig)
+
+	if err != nil {
+		return fmt.Errorf("failed to marshal sample config: %w", err)
+	}
+
+	file, err := os.Create(SampleConfigPath)
+
+	if err != nil {
+		return fmt.Errorf("failed to create config file: %w", err)
+	}
+
+	defer file.Close()
+
+	// Write the sample configuration to the file
+	_, err = file.Write(configData)
+
+	if err != nil {
+		return fmt.Errorf("failed to write sample config to file: %w", err)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"github.com/spf13/cobra"
+)
 
+func main() {
+	// chaindex create-config: create a sample config file (config.toml.sample)
+
+	var rootCmd = &cobra.Command{}
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "create-config",
+		Short: "Create a sample config file",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := createSampleConfig(); err != nil {
+				cmd.Println("Error creating sample config:", err)
+			}
+		},
+	})
+
+	if err := rootCmd.Execute(); err != nil {
+		rootCmd.Println("Error executing command:", err)
+	} else {
+		rootCmd.Println("Command executed successfully.")
+	}
 }
